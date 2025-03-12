e5352431fd3c984afdfa60421255f7de4050ffe4
Wenshuo Wang <wenshuow@uci.edu>
2025-03-12 04:55:01 +0000

    hw09

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -57,9 +57,8 @@ void print_path(const vector<int>& path, int total)
     for (size_t i = 0; i < path.size(); ++i) {
         cout << path[i];
         if (i != path.size() - 1) {
-            cout << " ";
+            cout << " "; 
         }
     }
-    cout << "\n";  
-    cout << "Total cost is " << total << "\n";  
+    cout << " \nTotal cost is " << total << "\n"; 
 }
--- src/ladder.cpp
+++ src/ladder.cpp
@@ -102,7 +102,7 @@ void load_words(set<string> & word_list, const string& file_name)
 void print_word_ladder(const vector<string>& ladder) 
 {
     if (ladder.empty()) {
-        cout << "No word ladder found" << endl;
+        cout << "No word ladder found." << endl;
         return;
     }
 
